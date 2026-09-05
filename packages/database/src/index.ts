25b22ab3b98454bfbafdaa04b5395038d830344a
Danny <120275321+Daniel419797@users.noreply.github.com>
2026-09-05 10:22:45 +0100

    feat(p1): export database infrastructure primitives

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,12 @@
 export { createDatabaseClient, type DatabaseClient, type DatabaseClientOptions } from "./client.js";
 export { isDatabaseReady } from "./health.js";
 export { withTransaction, type DatabaseTransaction } from "./transaction.js";
+export {
+  beginIdempotencyRecord,
+  enqueueOutboxEvent,
+  writeAuditLog,
+  type AuditLogInput,
+  type BeginIdempotencyInput,
+  type OutboxEventInput,
+} from "./infrastructure.js";
 export { Prisma } from "./generated/prisma/client.js";
